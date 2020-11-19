9cd4903c1bfa475471e8236b6193f0056d4e48b4
nappernick <nickfmatthews@gmail.com>
2020-11-19 13:16:45 -0500

    Working on csrf

--- frontend/stormlight/src/store/csrf.js
+++ frontend/stormlight/src/store/csrf.js
@@ -3,5 +3,14 @@ import Cookies from "js-cookie"
 const fetch = async (url, options = {}) => {
     if (!options.headers) options.headers = {}
     if (!options.method) options.method = 'GET'
+    if (options.method !== "GET") {
+        options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    }
+    const res = await window.fetch(url, options)
+    const contentType = res.headers.get('content-type')
+    if (contentType && contentType.includes('application/json')) res.data = await res.json()
+    if (res.status >= 400) throw (res)
 
+    return res
 }
