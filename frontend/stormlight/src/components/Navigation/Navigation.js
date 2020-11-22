eb75c87a80d852b06b457addc30d894c723583d4
nappernick <nickfmatthews@gmail.com>
2020-11-22 11:40:32 -0500

    Finished SignupLink

--- frontend/stormlight/src/components/Navigation/Navigation.js
+++ frontend/stormlight/src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { NavLink, useHistory, Redirect } from 'react-router-dom';
+import { useHistory, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import "./Navigation.css"
 import LoginLink from './LoginLink';
+import SignupLink from './SignupLink';
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
@@ -16,11 +17,7 @@ function Navigation({ isLoaded }) {
     } else if (history.location.pathname === "/signup") {
         sessionLinks = <LoginLink />
     } else if (history.location.pathname === '/login') {
-        sessionLinks = (
-            <div className="signup">
-                <NavLink to="/signup">Signup</NavLink>
-            </div>
-        )
+        sessionLinks = <SignupLink />
     }
 
     const handleClick = (e) => {
---
+++ frontend/stormlight/src/components/Navigation/SignupLink.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+
+function SignupLink() {
+    return (
+        <div className="signup">
+            <NavLink to="/signup">Signup</NavLink>
+        </div>
+    )
+}
+
+export default SignupLink
