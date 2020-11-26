043bdff814c1e99c11f321422f412a8871462c13
nappernick <nickfmatthews@gmail.com>
2020-11-25 22:23:21 -0500

    Got routes figured

--- backend/config/index.js
+++ backend/config/index.js
@@ -11,4 +11,8 @@ module.exports = {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
   },
+  apiConfig: {
+    finnHubApi: process.env.FINHUB_API_KEY,
+    alphaApi: process.env.ALPHA_API_KEY,
+  }
 };
--- backend/routes/api/stocks.js
+++ backend/routes/api/stocks.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { apiConfig } = require("../../config");
+const fetch = require("cross-fetch")
 const asyncHandler = require("express-async-handler");
 require('dotenv').config();
 const { Stock } = require("../../db/models");
+const { json } = require("express");
 
+const { finnHubApi, alphaApi } = apiConfig
 
 const router = express.Router()
 
@@ -27,11 +30,22 @@ router.get('/:id([0-9]+)', asyncHandler(async (req, res, next) => {
     })
 }))
 
-router.get("/:ticker(w+)", asyncHandler(async (req, res) => {
+router.get("/:ticker(\\w+)$", asyncHandler(async (req, res) => {
+    const { ticker } = req.params;
     let stocks = await fetch(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${finnHubApi}`)
-    console.log(stocks)
+    let test = await stocks.json()
     return res.json({
-        stocks,
+        test,
+    })
+}))
+
+router.get("/:interval(\\d{0,2})(\\w+)/:ticker(\\w+)$", asyncHandler(async(req, res) => {
+    const {interval, ticker} = req.params;
+    console.log(interval, ticker)
+    let rez = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=${interval}min&outputsize=compact&apikey=${alphaApi}`)
+    let stock = await rez.json()
+    return res.json({
+        stock,
     })
 }))
 
