56e45e83ae24921160e3b537c85f0d3a9ff5cc94
nappernick <nickfmatthews@gmail.com>
2021-02-09 22:02:13 -0500

    Made story image & title clickable

--- frontend/src/components/Dashboard/News/Story.js
+++ frontend/src/components/Dashboard/News/Story.js
@@ -4,11 +4,15 @@ function Story({ story }) {
     return (
         <div className="story__container">
             <div className="story__text">
-                <h2 className="story__headline">{story.headline}</h2>
+                <a href={story.url} target="_blank" rel="noopener noreferrer" >
+                    <h2 className="story__headline">{story.headline}</h2>
+                </a>
                 <h4 className="story__summary">{story.summary}</h4>
             </div>
             <div className="story__image">
-                <img src={story.image} alt="Story's Image" />
+                <a href={story.url}>
+                    <img src={story.image} alt="Story's Image" />
+                </a>
             </div>
         </div>
     )
