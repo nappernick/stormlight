60f2eb9b541429f7f4d37043caccf938cd0d1f49
nappernick <nickfmatthews@gmail.com>
2021-02-11 13:15:51 -0500

    Watchlist seed for demo is created

--- backend/db/seeders/20210211181211-demo-watchlist.js
+++ backend/db/seeders/20210211181211-demo-watchlist.js
@@ -4,42 +4,28 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Stocks', [
       {
-        ticker: "AAPL",
-        buyPrice: 117.34,
-        numStock: 3,
+        ticker: "XONE",
         userId: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        ticker: "TWTR",
-        buyPrice: 44.69,
-        numStock: 6,
+        ticker: "LIZI",
         userId: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        ticker: "TSLA",
-        buyPrice: 490.4,
-        numStock: 1,
+        ticker: "TLRY",
         userId: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-      {
-        ticker: "GE",
-        buyPrice: 9.73,
-        numStock: 13,
-        userId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Stocks', {
+    await queryInterface.bulkDelete('Watchlists', {
       userId: 1
     }, {});
   }
