b541f448e9418fc26a61cc5c774910ab58db2fc1
nappernick <nickfmatthews@gmail.com>
2021-02-11 15:49:08 -0500

    Watchlist backend routes complete, beginning work on watchlist redux store

--- backend/routes/api/watchList.js
+++ backend/routes/api/watchList.js
@@ -10,8 +10,39 @@ const { json } = require("express");
 
 const router = express.Router()
 
-router.post("/:userId([0-9]+)")
+router.post("/user/:userId([0-9]+)/ticker/:ticker(\\w+)$", asyncHandler(async (req, res, next) => {
+    const { userId, ticker } = req.params
+    ticker = ticker.toUpperCase()
+    const watchList = await Watchlist.create({
+        ticker,
+        userId
+    })
+    return res.json({ watchList })
+}))
 
+router.delete('/user/:userId([0-9]+)/ticker/:ticker(\\w+)$', asyncHandler(async (req, res, next) => {
+    const { userId, ticker } = req.params
+    ticker = ticker.toUpperCase()
+    const toDelete = await Watchlist.findOne({
+        where: {
+            userId,
+            ticker
+        }
+    })
+    await Watchlist.destroy({
+        where: {
+            id: toDelete.dataValues.id
+        }
+    })
+}))
+
+router.get("/user/:userId([0-9]+)", asyncHandler(async (req, res, next) => {
+    const { userId } = req.params
+    const watchList = Watchlist.findAll({ where: { userId } })
+    return res.json({
+        watchList
+    })
+}))
 
 
 module.exports = router;
